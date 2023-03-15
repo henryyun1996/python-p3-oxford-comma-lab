3deb3eea55e791d75246b6766ed3adb30b7552cf
Henry <henrysungyun@gmail.com>
2023-03-15 10:36:15 -0700

    come back

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return str(items[0])
+    elif len(items) == 2:
+        return (f"{items[0]} and {items[1]}")
+    else:
+        oxford_items = [str(item) for item in items[:-1]]
+        oxford_items.append(f"and {items[-1]}")
+        return ", ".join(oxford_items)
